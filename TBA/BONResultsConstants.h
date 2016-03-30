57143b0f8f10c6989421b0d0fb601cdfc4fdafb3
fleish007 <cfleish@gmail.com>
2016-03-30 17:36:23 -0400

    framework for message and dynamic results

--- TBA/BONResultsConstants.h
+++ TBA/BONResultsConstants.h
@@ -12,9 +12,25 @@
 @interface BONResultsConstants : NSObject
 
 // Label Layout
-extern NSInteger const LEADING_CONSTANT;
-extern NSInteger const TRAILING_CONSTANT;
-extern NSInteger const TOP_CONSTANT;
-extern CGFloat const HEIGHT_MULTIPLIER;
+extern NSInteger const LABEL_LEADING_CONSTANT;
+extern NSInteger const LABEL_TRAILING_CONSTANT;
+extern NSInteger const LABEL_TOP_CONSTANT;
+extern CGFloat const LABEL_HEIGHT_MULTIPLIER;
+
+// Results View Layout
+extern NSInteger const RESULTS_VIEW_LEADING_CONSTANT;
+extern NSInteger const RESULTS_VIEW_TRAILING_CONSTANT;
+extern NSInteger const RESULTS_VIEW_TOP_CONSTANT;
+extern NSInteger const RESULTS_VIEW_BOTTOM_CONSTANT;
+
+// Results Row Label Layout
+extern NSInteger const RESULTS_ROW_LABEL_LEFT;
+extern NSInteger const RESULTS_ROW_LABEL_RIGHT;
+extern NSInteger const RESULTS_ROW_LABEL_TOP;
+extern NSInteger const RESULTS_ROW_LABEL_BOTTOM;
+
+// Results Data
+extern NSString *const QUESTION_KEY;
+extern NSString *const ANSWER_KEY;
 
 @end
--- TBA/BONResultsConstants.m
+++ TBA/BONResultsConstants.m
@@ -11,9 +11,25 @@
 @implementation BONResultsConstants
 
 // Label Layout
-NSInteger const LEADING_CONSTANT = 20;
-NSInteger const TRAILING_CONSTANT = 20;
-NSInteger const TOP_CONSTANT = 20;
-CGFloat const HEIGHT_MULTIPLIER = 0.4;
+NSInteger const LABEL_LEADING_CONSTANT = 20;
+NSInteger const LABEL_TRAILING_CONSTANT = -20;
+NSInteger const LABEL_TOP_CONSTANT = 20;
+CGFloat const LABEL_HEIGHT_MULTIPLIER = 0.4;
+
+// Results View Layout
+NSInteger const RESULTS_VIEW_LEADING_CONSTANT = 20;
+NSInteger const RESULTS_VIEW_TRAILING_CONSTANT = -20;
+NSInteger const RESULTS_VIEW_TOP_CONSTANT = 50;
+NSInteger const RESULTS_VIEW_BOTTOM_CONSTANT = -20;
+
+// Results Row Label Layout
+NSInteger const RESULTS_ROW_LABEL_LEFT = 5;
+NSInteger const RESULTS_ROW_LABEL_RIGHT = -5;
+NSInteger const RESULTS_ROW_LABEL_TOP = 5;
+NSInteger const RESULTS_ROW_LABEL_BOTTOM = -5;
+
+// Results Data
+NSString *const QUESTION_KEY = @"Question";
+NSString *const ANSWER_KEY = @"Answer";
 
 @end
