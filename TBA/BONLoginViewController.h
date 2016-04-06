06bd8d6c0e55819ff9f5ffb149072f3d6ee60943
fleish007 <cfleish@gmail.com>
2016-04-06 10:23:10 -0400

    view hierarchy

---
+++ TBA/BONLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  BONLoginViewController.h
+//  TBA
+//
+//  Created by christopher fleisher on 4/6/16.
+//  Copyright © 2016 flatiron school. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BONLoginViewController : UIViewController
+
+@end
---
+++ TBA/BONLoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  BONLoginViewController.m
+//  TBA
+//
+//  Created by christopher fleisher on 4/6/16.
+//  Copyright © 2016 flatiron school. All rights reserved.
+//
+
+#import "BONLoginViewController.h"
+
+@interface BONLoginViewController ()
+
+@end
+
+@implementation BONLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
