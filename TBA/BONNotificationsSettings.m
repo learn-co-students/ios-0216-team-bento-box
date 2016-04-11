916677266e4149c68d9a00fdfb8d36c2e2819881
opusmagnum77 <daniel.adeyanju@gmail.com>
2016-04-11 18:11:54 -0400

    logic for notifications

--- TBA/BONNotificationsSettings.m
+++ TBA/BONNotificationsSettings.m
@@ -22,6 +22,7 @@
 }
 
 -(void)setBreakfastNotification:(NSDate *)date {
+    [self deleteNotification:@"breakfast"];
     UILocalNotification * breakfastNotif = [[UILocalNotification alloc] init];
     [breakfastNotif setAlertBody:@"It's around breakfast time, log your meal!"];
    
@@ -30,6 +31,7 @@
     breakfastNotif.repeatInterval = NSCalendarUnitWeekday;
     NSDate * breakfastTimeFromComponents = [myCal dateFromComponents:breakfastTime];
     [breakfastNotif setFireDate: breakfastTimeFromComponents];
+    [breakfastNotif.userInfo setValue:@"breakfast" forKey:@"uid"];
     
     
     
@@ -37,11 +39,56 @@
 
     
 }
+
+
+-(void)setMealType:(NSString *)  mealType Notification:(NSDate *)date {
+    [self deleteNotification:mealType];
+    UILocalNotification * breakfastNotif = [[UILocalNotification alloc] init];
+    NSString * alertBody = [NSString stringWithFormat: @"It's around %@ time, log your meal!", mealType];
+    [breakfastNotif setAlertBody:alertBody];
+    
+    NSCalendar * myCal = [NSCalendar currentCalendar];
+    NSDateComponents * breakfastTime = [myCal components:NSCalendarUnitHour| NSCalendarUnitMinute fromDate:date];
+    breakfastNotif.repeatInterval = NSCalendarUnitWeekday;
+    NSDate * breakfastTimeFromComponents = [myCal dateFromComponents:breakfastTime];
+    [breakfastNotif setFireDate: breakfastTimeFromComponents];
+    [breakfastNotif.userInfo setValue:mealType forKey:@"uid"];
+    
+    
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:breakfastNotif];
+    
+    
+}
+
+
+//helper method for setNotification, deletes existing notification for mealtype
+-(void)deleteNotification: (NSString *) uidtodelete {
+    UIApplication *app = [UIApplication sharedApplication];
+    NSArray *eventArray = [app scheduledLocalNotifications];
+    
+    NSLog(@"%@", eventArray);
+    
+    for (int i=0; i<[eventArray count]; i++)
+    {
+        UILocalNotification* oneEvent = [eventArray objectAtIndex:i];
+        NSDictionary *userInfoCurrent = oneEvent.userInfo;
+        NSString *uid=[NSString stringWithFormat:@"%@",[userInfoCurrent valueForKey:@"uid"]];
+        if ([uid isEqualToString:uidtodelete])
+        {
+            //Cancelling local notification
+            NSLog(@"notification found");
+            [app cancelLocalNotification:oneEvent];
+            break;
+        }
+    }
+}
 - (IBAction)submit:(id)sender {
     
     NSDate * breakfastTimePicked = self.breakfastNotifPicker.date;
     
-    [self setBreakfastNotification:breakfastTimePicked];
+    //[self setBreakfastNotification:breakfastTimePicked];
+    [self setMealType:@"breakfast" Notification:breakfastTimePicked];
 }
 @end
 
