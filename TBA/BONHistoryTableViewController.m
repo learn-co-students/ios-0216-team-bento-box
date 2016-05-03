49a50269b1c422c17dd9ff82e1eb9b69b449866c
ArielSD <ascottdicker@dosomething.org>
2016-05-02 20:06:19 -0400

    Stashing before merging to master

--- TBA/BONHistoryTableViewController.m
+++ TBA/BONHistoryTableViewController.m
@@ -113,8 +113,6 @@
     [parentViewController displayContentController:parentViewController.childViewControllers.firstObject];
 }
 
-
-
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
