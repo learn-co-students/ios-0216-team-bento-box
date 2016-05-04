655dcc6a80ce069b47982ca7ca926f92bb8380b7
opusmagnum77 <daniel.adeyanju@gmail.com>
2016-05-03 20:32:15 -0400

    Add New Meal Button to nav drawer

--- TBA/BONHamburgerViewController.m
+++ TBA/BONHamburgerViewController.m
@@ -9,6 +9,8 @@
 #import "BONHamburgerViewController.h"
 #import "BONContainerViewController.h"
 #import "BONFirebaseClient.h"
+#import "BONWhenViewController.h"
+#import "BONChildViewController.h"
 
 @interface BONHamburgerViewController ()
 @property (nonatomic, strong)NSArray * arr;
@@ -81,10 +83,18 @@
     [insightsButton.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:20].active=YES;
     [insightsButton setTitle:@"Insights" forState:UIControlStateNormal];
     
+    UIButton * newMealButton = [[UIButton alloc]init];
+    [self.view addSubview:newMealButton];
+    newMealButton.translatesAutoresizingMaskIntoConstraints=NO;
+    [newMealButton.topAnchor constraintEqualToAnchor:insightsButton.bottomAnchor].active=YES;
+    [newMealButton.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:20].active=YES;
+    [newMealButton setTitle:@"New Meal" forState:UIControlStateNormal];
+    [newMealButton addTarget:self action:@selector(newMeal) forControlEvents:UIControlEventTouchUpInside];
+    
     UIButton * logoutButton = [[UIButton alloc]init];
     [self.view addSubview:logoutButton];
     logoutButton.translatesAutoresizingMaskIntoConstraints=NO;
-    [logoutButton.topAnchor constraintEqualToAnchor:insightsButton.bottomAnchor].active=YES;
+    [logoutButton.topAnchor constraintEqualToAnchor:newMealButton.bottomAnchor].active=YES;
     [logoutButton.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:20].active=YES;
     [logoutButton setTitle:@"Logout" forState:UIControlStateNormal];
     [logoutButton addTarget:self action:@selector(goToVC:) forControlEvents:UIControlEventTouchUpInside];
@@ -122,6 +132,48 @@
 -(void)logOutButtonHit {
     Firebase *rootReference = [[Firebase alloc] initWithUrl:@"https://crackling-fire-2900.firebaseio.com"];
     [rootReference unauth];
+}
+
+-(void)newMeal {
+    
+    BONContainerViewController *parentViewController = (BONContainerViewController *)self.parentViewController;
+    
+    parentViewController.userMeal = [NSEntityDescription insertNewObjectForEntityForName:@"Meal"
+                                                                  inManagedObjectContext:parentViewController.localDataStore.managedObjectContext];
+    
+    parentViewController.userMeal.createdAt = [NSDate date];
+    
+    parentViewController.viewCounter = 0;
+    [parentViewController.childViewControllers removeAllObjects];
+    
+    UIStoryboard *arielStoryboard = [UIStoryboard storyboardWithName:@"Ariel's Storyboard"
+                                                              bundle:nil];
+    UIStoryboard *whenStoryboard = [UIStoryboard storyboardWithName:@"BONWhenView"
+                                                             bundle:nil];
+    UIStoryboard *history= [UIStoryboard storyboardWithName:@"BONHistoryStoryboard"
+                                                     bundle:nil];
+    
+    BONChildViewController *whatViewController = [BONChildViewController new];
+    BONChildViewController *whenViewController = [BONChildViewController new];
+    
+    [whatViewController getQuestionLabel];
+    [whenViewController getQuestionLabel];
+    
+    whatViewController.questionLabel.textColor = [UIColor whiteColor];
+    whatViewController.questionLabel.text = @"What did you eat?";
+    
+    whenViewController.questionLabel.textColor = [UIColor whiteColor];
+    whenViewController.questionLabel.text = @"When did you eat?";
+    
+    [parentViewController.childViewControllers addObject:[arielStoryboard instantiateViewControllerWithIdentifier:@"welcome"]];
+    [parentViewController.childViewControllers addObject:[whenStoryboard instantiateViewControllerWithIdentifier:@"when"]];
+    [parentViewController.childViewControllers addObject:[whenStoryboard instantiateViewControllerWithIdentifier:@"what"]];
+    [parentViewController.childViewControllers addObject:[arielStoryboard instantiateViewControllerWithIdentifier:@"whereViewController"]];
+    [parentViewController.childViewControllers addObject:[whenStoryboard instantiateViewControllerWithIdentifier:@"how"]];
+    [parentViewController.childViewControllers addObject:[history instantiateViewControllerWithIdentifier:@"historyTableVC"]];
+    
+    [parentViewController displayContentController:parentViewController.childViewControllers.firstObject];
+
 }
 
 @end
