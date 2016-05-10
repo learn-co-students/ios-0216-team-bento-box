b0a9f5f6045d8f94e0753341f97d8a47e1d901b6
opusmagnum77 <daniel.adeyanju@gmail.com>
2016-05-10 14:05:05 -0400

    Fix history vc contraints

--- TBA/BONWhenViewController.m
+++ TBA/BONWhenViewController.m
@@ -220,6 +220,13 @@
 - (void)setFontsStyle {
     
     self.whenDidYouEatQuestionLabel.font = [UIFont fontWithName:@"Baskerville" size:20];
+    self.whenDidYouEatQuestionLabel.textAlignment = NSTextAlignmentCenter;
+
+//    self.whenDidYouEatQuestionLabel.translatesAutoresizingMaskIntoConstraints=NO;
+//    [self.whenDidYouEatQuestionLabel.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:60].active = YES;
+//    [self.whenDidYouEatQuestionLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+//    [self.whenDidYouEatQuestionLabel.widthAnchor constraintEqualToAnchor: self.view.widthAnchor].active = YES;
+    
     self.submitButton.titleLabel.font = [UIFont fontWithName:@"Baskerville" size:20];
     self.backButton.titleLabel.font = [UIFont fontWithName:@"Baskerville" size:20];
     [self.timePicker setValue:[UIColor colorWithRed:255.0f/255.0f
